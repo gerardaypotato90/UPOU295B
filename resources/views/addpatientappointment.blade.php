ac738f39d5ba116fbbc963b6f6e30d42696d334f
gerardaypotato90 <gerard.alainp@gmail.com>
2023-05-02 22:53:22 +0800

    labels

--- resources/views/addpatientappointment.blade.php
+++ resources/views/addpatientappointment.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Add New Patient') }}
+            {{ __('Book Appointment for Patient') }}
         </h2>
     </x-slot>
     <script src="https://code.highcharts.com/highcharts.js"></script>
