6c03a9644a2c262e4de6c02ad59e5459ddb5739b
gerardaypotato90 <gerard.alainp@gmail.com>
2023-05-06 22:13:53 +0800

    remove admin option

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,7 +37,6 @@
                 <select name="usertype" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full">
                     <option value="Patient">Patient</option>
                     <option value="Doctor">Doctor</option>
-                    <option value="Admin">Admin</option>
                 </select>
 
             </div>
